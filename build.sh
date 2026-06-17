2cbad3a05dfd80d2bb8abf2dd075ecf0d3c3e55c
Lejla Catovic <lejla.catovic18@gmail.com>
2026-06-17 10:18:35 +0200

    create superuser in build

--- build.sh
+++ build.sh
@@ -4,3 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from accounts.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(username='admin', email='lejla.catovic18@gmail.com', password='Admin1234!', role='admin')
+    print('Superuser kreiran!')
+else:
+    print('Vec postoji.')
+"
